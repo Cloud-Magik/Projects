820735b481c989dc94d4b9bdf310b3943d8f5ebb
Jxgonzalez91 <55545686+Jxgonzalez91@users.noreply.github.com>
2019-10-07 16:28:27 -0400

    Add files via upload

--- RoomPackages.py
+++ RoomPackages.py
@@ -1,12 +1,19 @@
-import pprint
+class roomPackages:
+    def _init_(self, rooms, amenities):
+        self.rooms = rooms
+        self.amenities = amenities
 
-RoomPackages = {
-    "1.Standard": "Single twin bed:$50",
-    "2.Standard for two": "two twin beds:$75",
-    "3.Premiere": "Queen bed:$125",
-    "4.Deluxe": "King bed:$175",
-    "5.Honeymoon Suite": "Custom king bed:$225",
-    "6.Master Suite": "Custom king bed with butler:$300",
-    "7.Presidential Suite": "California king bed with butler:$500"
-}
-pprint.pprint(RoomPackages)
+        self.room_types = {'1. Standard, w/Full size Bed': 10,
+                           '2. Standard for two, w/ two Full size beds': 20,
+                           '3. Deluxe, w/Queen bed': 30,
+                           '4. Premiere, w/ King bed': 40,
+                           '5. Paradise Honeymoon Suite, w/ California King Bed and Jacuzzi Bath': 50,
+                           '6. Paradise Master Suite, w/ California King Bed, Balcony w/ HotTub': 60
+                           '7. Paradise Presidential Suite, w/ Balcony and Infinity Pool': 70}
+        self.room_price = {'1. Standard, w/Full size Bed': $85
+                           '2. Standard for two, w/ two Full size beds': $100,
+                           '3. Deluxe, w/Queen bed': $125,
+                           '4. Premiere, w/ King bed': $175,
+                           '5. Paradise Honeymoon Suite, w/ California King Bed, and Jacuzzi Bath': $225,
+                           '6. Paradise Master Suite, w/ California King Bed, Balcony w/ HotTub and Butler Services': $300
+                           '7. Paradise Presidential Suite, w/ Balcony, Infinity Pool and Butler Services': $500}
