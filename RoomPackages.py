bf18ad16b20a22e5a8f7716b9acacfb5eac9c3cd
Jxgonzalez91 <55545686+Jxgonzalez91@users.noreply.github.com>
2019-10-07 16:30:59 -0400

    Add files via upload

--- RoomPackages.py
+++ RoomPackages.py
@@ -8,9 +8,9 @@ class roomPackages:
                            '3. Deluxe, w/Queen bed': 30,
                            '4. Premiere, w/ King bed': 40,
                            '5. Paradise Honeymoon Suite, w/ California King Bed and Jacuzzi Bath': 50,
-                           '6. Paradise Master Suite, w/ California King Bed, Balcony w/ HotTub': 60
+                           '6. Paradise Master Suite, w/ California King Bed, Balcony w/ HotTub': 60,
                            '7. Paradise Presidential Suite, w/ Balcony and Infinity Pool': 70}
-        
+
         self.room_price = {'1. Standard, w/Full size Bed': $85
                            '2. Standard for two, w/ two Full size beds': $100,
                            '3. Deluxe, w/Queen bed': $125,
