3d575a7bdc4a8bfe3174afd5c37f28fb6c06cb61
magikcloud <55546610+magiknimbus@users.noreply.github.com>
2019-10-14 19:06:16 -0400

    update

--- Connect.py
+++ Connect.py
@@ -20,40 +20,7 @@ try:
         cursor.close()
 
     createDB()
-    # def retrieveUserInfo(id):
-    #     cursor = conn.cursor()
-    #     cursor.execute(
-    #         f"SELECT first_name, last_name, phone_number FROM cohort2 WHERE user_id={id}"
-    #     )
-    #     rows = cursor.fetchall()
-    #     if(rows):
-    #         for row in rows:
-    #             print(f'''
-    #            First Name: {row[0]}
-    #            Last Name: {row[1]}
-    #            Phone: {row[2]}
-    #            ''')
-    #             cursor.close()
-    #     else:
-    #         print("Your database is empty")
-    # userId = int(input("Enter the customer's id to pull their info >> "))
-    # retrieveUserInfo(userId)
+    
 except(Exception, psycopg2.Error) as error:
     print("Error while fetching data from PostgreSQL", error)
 
-
-#         print('PostgreSQL database version:')
-#         cur.execute('SELECT version()')
-
-#         db_version=cur.fetchone()
-#         print(db_version)
-#         cur.close()
-#     except (Exception, psycopg2.DatabaseError) as Error:
-#         print(Error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#             print('Database connection closed.')
-
-# if __name__== '__main__':
-#     connect()
