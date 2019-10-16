dafbaf4e50c4b41036e2b833d7de438daccbec1e
magiknimbus <55546610+magiknimbus@users.noreply.github.com>
2019-10-16 19:33:17 -0400

    burner insert test

--- burnerScript.py
+++ burnerScript.py
@@ -4,84 +4,89 @@ import random
 import calendar
 import getpass
 from burner import *
-# ---------------------------------------------------------------
-# ******************Requirements for project********************
-# fill brackets with [X] when requirements are fulfilled
-# use comments [X]
-# exceptions for inputs; make the code unbreakable []
-# save info and pull information from database []
-# calculations clean []
-# CRUD operations (create,read,update,delete)[]
-# use classes []
-# use objects[]
-# use loops[]
-# use arrays[]
-# use Lists[]
-# use conditionals[]
-# use functions[]
-# use file imports[]
-# use discounts or incentive for project[]
-# ---------------------------------------------------------------
-
+import psycopg2
+try:
+    conn = psycopg2.connect(
+        database="project",
+        user="postgres",
+        password="0000",
+        host="127.0.0.1",
+        port="5432"
+    )
 # INTRO; Title page, Create user or login
-Paradisefalls()
+# Paradisefalls()
 
 
-def main():
+# def main():
 
-    while True:
+#     while True:
 
-        answer = input(
-            "\n\n\nWould you like to login to an existing account or register? (Login/Register): ")
-        if answer[:1].upper() == 'R':
-            register_user()
+#         answer = input(
+#             "\n\n\nWould you like to login to an existing account or register? (Login/Register): ")
+#         if answer[:1].upper() == 'R':
+#             register_user()
 
-        elif answer[:1].upper() == 'L':
-            log_in()
-            print("Succesful login!")
-            break
-        else:
-            print("Invalid entry")
-
-
-class User:
-    def __init__(self, username=None, password=None):
-        self.username = username
-        self.password = password
+#         elif answer[:1].upper() == 'L':
+#             log_in()
+#             print("Succesful login!")
+#             break
+#         else:
+#             print("Invalid entry")
 
 
-def register_user():
-    print("\nLets register you")
-    user = User()
-    user.username = input("Enter Username to register: ").lower()
-    user.password = getpass.getpass("Enter your password: ")
-
-    csvfile = open('users.csv', 'a+')
-    csvfile.write(f"{user.username},{user.password}\n")
-    csvfile.close()
+# class User:
+#     def __init__(self, username=None, password=None):
+#         self.username = username
+#         self.password = password
 
 
-def log_in():
-    print("Login info:")
-    username = input("Login with Username: ").lower()
-    password = getpass.getpass("Enter your password: ")
+# def register_user():
+#     print("\nLets register you")
+#     user = User()
+#     user.username = input("Enter Username to register: ").lower()
+#     user.password = getpass.getpass("Enter your password: ")
 
-    with open('users.csv') as csvfile:
-        CSVData = csv.reader(csvfile, delimiter=',')
-
-        userExists = False
-        for row in CSVData:
-
-            if username == row[0] and password == row[1]:
-
-                userExists = True
-                return True
-                break
-
-        if not userExists:
-            print("User doesn't exist!")
-            log_in()
-    csvfile.close()
+#     csvfile = open('users.csv', 'a+')
+#     csvfile.write(f"{user.username},{user.password}\n")
+#     csvfile.close()
 
 
-main()
+# def log_in():
+#     print("Login info:")
+#     username = input("Login with Username: ").lower()
+#     password = getpass.getpass("Enter your password: ")
+
+#     with open('users.csv') as csvfile:
+#         CSVData = csv.reader(csvfile, delimiter=',')
+
+#         userExists = False
+#         for row in CSVData:
+
+#             if username == row[0] and password == row[1]:
+
+#                 userExists = True
+#                 return True
+#                 break
+
+#         if not userExists:
+#             print("User doesn't exist!")
+#             log_in()
+#     csvfile.close()
+
+
+# main()
+def insertUserInfo(email, first_name, last_name, username, pass_word):
+    cursor = conn.cursor()
+    cursor.execute(
+        f"INSERT INTO User (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word})"
+    )
+    conn.commit()
+    print("Record inserted successfully")
+    cursor.close()
+    email = input("Please enter email to register:")
+    first_name = input("Enter first name:")
+    last_name = input("Enter last name:")
+    username = input("Enter username for account:")
+    pass_word = input("Enter password for account:")
+
+insertUserInfo(email, first_name, last_name, username, pass_word)
