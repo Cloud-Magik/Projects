e9d8f5a2fea97d33a0a9cba2330bc03b5a65a25d
magiknimbus <55546610+magiknimbus@users.noreply.github.com>
2019-10-16 21:09:55 -0400

    test login auth

--- inserts.py
+++ inserts.py
@@ -11,22 +11,38 @@ try:
 
 # New user registration info inserted into postgreSQL database
 
-    def insertNewUser(email, first_name, last_name, username, pass_word):
+    # def insertNewUser(email, first_name, last_name, username, pass_word):
+    #     cursor = conn.cursor()
+    #     cursor.execute(
+    #         f"INSERT INTO User (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word})"
+    #     )
+    #     conn.commit()
+    #     print("Record inserted successfully")
+    #     cursor.close()
+    #     email = input("Please enter email to register:")
+    #     first_name = input("Enter first name:")
+    #     last_name = input("Enter last name:")
+    #     username = input("Enter username for account:")
+    #     pass_word = input("Enter password for account:")
+
+    #     insertNewUser(email, first_name, last_name, username, pass_word)
+
+# Login Authentication where checks if you have account in postgreSQL database
+
+    def Login(username, pass_word):
+        print("""****** PLEASE LOG IN ******
+        """)
+        username = input("Username: ")
+        pass_word = input("Password: ")
         cursor = conn.cursor()
         cursor.execute(
-            f"INSERT INTO User (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word})"
-        )
-        conn.commit()
-        print("Record inserted successfully")
+            f"SELECT * FROM users WHERE username='{username}' AND pass_word='{pass_word}'")
+        rows = cursor.fetchall()
+        if(rows):
+            print("You have succesfully logged in!")
+        else:
+            print("Wrong credentials")
         cursor.close()
-        email = input("Please enter email to register:")
-        first_name = input("Enter first name:")
-        last_name = input("Enter last name:")
-        username = input("Enter username for account:")
-        pass_word = input("Enter password for account:")
-
-        insertNewUser(email, first_name, last_name, username, pass_word)
-
 # Reservation info inserted into postgreSQL database
 
     def insertReservation(arrival_date, departure_date, room_package):
