6410db90feb58fc695cfe1a9abbaee453a6c24fc
magikcloud <55546610+magiknimbus@users.noreply.github.com>
2019-10-17 10:28:01 -0400

    testing functions with script structure

--- burner.py
+++ burner.py
@@ -2,7 +2,41 @@ def Paradisefalls():
     Title = open("Title.txt")
     print(Title.read())
     Title.close()
-
+Paradisefalls()
 
 def test():
     print('test')
+
+def insertNewUser(email, first_name, last_name, username, pass_word):
+        cursor = conn.cursor()
+        cursor.execute(
+            f"INSERT INTO users (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word}')"
+        )
+        conn.commit()
+        print("Record inserted successfully")
+        cursor.close()
+print("""\nPlease enter registration info for Paradise Falls
+    """)
+email = input("Please enter email to register:").lower()
+first_name = input("Enter first name:").lower()
+last_name = input("Enter last name:").lower()
+username = input("Enter username for account:").lower()
+pass_word = getpass.getpass("Enter password for account:").lower()
+
+insertNewUser(email, first_name, last_name, username, pass_word)
+
+def Login(username, pass_word):
+        cursor = conn.cursor()
+        cursor.execute(
+            f"SELECT * FROM users WHERE username='{username}' AND pass_word='{pass_word}'")
+        rows = cursor.fetchall()
+        if(rows):
+            print("You have succesfully logged in!")
+        else:
+            print("Wrong credentials")
+        cursor.close()
+print("""\nPlease enter Login info
+        """)
+username = input("Username: ").lower()
+pass_word = getpass.getpass("Password: ").lower()
+Login(username, pass_word)
--- burnerScript.py
+++ burnerScript.py
@@ -5,92 +5,46 @@ import calendar
 import getpass
 from burner import *
 import psycopg2
-try:
-    conn = psycopg2.connect(
-        database="project",
-        user="postgres",
-        password="0000",
-        host="127.0.0.1",
-        port="5432"
-    )
-# INTRO; Title page, Create user or login
-# Paradisefalls()
 
-
-# def main():
-
-#     while True:
-
-#         answer = input(
-#             "\n\n\nWould you like to login to an existing account or register? (Login/Register): ")
-#         if answer[:1].upper() == 'R':
-#             register_user()
-
-#         elif answer[:1].upper() == 'L':
-#             log_in()
-#             print("Succesful login!")
-#             break
-#         else:
-#             print("Invalid entry")
-
-
-# class User:
-#     def __init__(self, username=None, password=None):
-#         self.username = username
-#         self.password = password
-
-
-# def register_user():
-#     print("\nLets register you")
-#     user = User()
-#     user.username = input("Enter Username to register: ").lower()
-#     user.password = getpass.getpass("Enter your password: ")
-
-#     csvfile = open('users.csv', 'a+')
-#     csvfile.write(f"{user.username},{user.password}\n")
-#     csvfile.close()
-
-
-# def log_in():
-#     print("Login info:")
-#     username = input("Login with Username: ").lower()
-#     password = getpass.getpass("Enter your password: ")
-
-#     with open('users.csv') as csvfile:
-#         CSVData = csv.reader(csvfile, delimiter=',')
-
-#         userExists = False
-#         for row in CSVData:
-
-#             if username == row[0] and password == row[1]:
-
-#                 userExists = True
-#                 return True
-#                 break
-
-#         if not userExists:
-#             print("User doesn't exist!")
-#             log_in()
-#     csvfile.close()
-
-
-# main()
-
-    # def insertUserInfo(email, first_name, last_name, username, pass_word):
-    #     cursor = conn.cursor()
-    #     cursor.execute(
-    #         f"INSERT INTO users (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word}')"
-    #     )
-    #     conn.commit()
-    #     print("Record inserted successfully")
-    #     cursor.close()
-    # email = input("Please enter email to register:")
-    # first_name = input("Enter first name:")
-    # last_name = input("Enter last name:")
-    # username = input("Enter username for account:")
-    # pass_word = getpass.getpass("Enter password for account:")
-
-    # insertUserInfo(email, first_name, last_name, username, pass_word)
+Paradisefalls()
+def main():
+        while True:
     
+            answer = input(
+                "\n\n\nWould you like to login to an existing account or register? (Login/Register): ")
+            if answer[:1].upper() == 'R':
+                insertNewUser()
+
+            elif answer[:1].upper() == 'L':
+                Login()
+                print("Succesful login!")
+                break
+            else:
+                print("Invalid entry")
+# def main():
+try:
+        conn = psycopg2.connect(
+            database="project",
+            user="postgres",
+            password="0000",
+            host="127.0.0.1",
+            port="5432"
+        )
+
 except(Exception, psycopg2.Error) as error:
         print("Error while fetching data from PostgreSQL", error)
+# while True:
+
+#     answer = input(
+#         "\n\n\nWould you like to login to an existing account or register? (Login/Register): ")
+#     if answer[:1].upper() == 'R':
+#         insertNewUser()
+
+#     elif answer[:1].upper() == 'L':
+#         Login()
+#         print("Succesful login!")
+#         break
+#     else:
+#         print("Invalid entry")
+
+main()
