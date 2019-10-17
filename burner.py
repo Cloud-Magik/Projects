6f4a66a7b9f35215f29aedff27a60de38518efd1
magikcloud <55546610+magiknimbus@users.noreply.github.com>
2019-10-17 11:08:17 -0400

    try

--- burner.py
+++ burner.py
@@ -1,32 +1,41 @@
 import getpass
 import psycopg2
-def Paradisefalls():
-    Title = open("Title.txt")
-    print(Title.read())
-    Title.close()
-
-def test():
-    print('test')
-
-def insertNewUser(email, first_name, last_name, username, pass_word):
-    cursor = conn.cursor()
-    cursor.execute(
-        f"INSERT INTO users (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word}')"
+try:
+    conn = psycopg2.connect(
+        database="project",
+        user="postgres",
+        password="0000",
+        host="127.0.0.1",
+        port="5432"
     )
-    conn.commit()
-    print("Record inserted successfully")
-    cursor.close()
-print("""\nPlease enter registration info for Paradise Falls
-    """)
+    def Paradisefalls():
+        Title = open("Title.txt")
+        print(Title.read())
+        Title.close()
 
-def Login(username, pass_word):
-    cursor = conn.cursor()
-    cursor.execute(
-        f"SELECT * FROM users WHERE username='{username}' AND pass_word='{pass_word}'")
-    rows = cursor.fetchall()
-    if(rows):
-        print("You have succesfully logged in!")
-    else:
-        print("Wrong credentials")
-    cursor.close()
+    def test():
+        print('test')
 
+    def insertNewUser(email, first_name, last_name, username, pass_word):
+        cursor = conn.cursor()
+        cursor.execute(
+            f"INSERT INTO users (email, first_name, last_name, username, pass_word) VALUES ('{email}','{first_name}', '{last_name}', '{username}', '{pass_word}')"
+        )
+        conn.commit()
+        print("Record inserted successfully")
+        cursor.close()
+    print("""\nPlease enter registration info for Paradise Falls
+        """)
+
+    def Login(username, pass_word):
+        cursor = conn.cursor()
+        cursor.execute(
+            f"SELECT * FROM users WHERE username='{username}' AND pass_word='{pass_word}'")
+        rows = cursor.fetchall()
+        if(rows):
+            print("You have succesfully logged in!")
+        else:
+            print("Wrong credentials")
+        cursor.close()
+except(Exception, psycopg2.Error) as error:
+    print("Error while fetching data from PostgreSQL", error)
