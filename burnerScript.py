f3889870e66bacb27b4e662963274c95637f168d
Jxgonzalez91 <55545686+Jxgonzalez91@users.noreply.github.com>
2019-10-19 17:47:11 -0400

    Update burnerScript.py

--- burnerScript.py
+++ burnerScript.py
@@ -45,6 +45,67 @@ try:
             else:
                 print("Invalid entry")
                 x = 1
+        x = 1
+        while True:
+
+            answer = input(
+                "\n\n\nWould you like to book a reservation, update a reservation, view a reservation, or cancel a reservation? (book/update/view): ").upper()
+            if answer[:1].upper() == 'B':
+                print("""\n Welcome to Paradise Falls, please book now!:
+                """)
+                arrival_date = input("choose arrival date:").lower()
+                departure_date = input("choose departure date:").lower()
+                room_package = input("choose room:").lower()
+                x = 1
+            elif answer[:1].upper() == 'U':
+                print("""\n  Update Reservation now!:
+                      """)
+                answer = input(
+                    "\n\n\n What would you like to update? arrival date, departure date, or room package  (book/update/view): ").upper()
+                if answer[:1].upper() == 'A':
+                    arrival_date = input("choose arrival date:").lower()
+                elif answer[:1].upper() == 'D':
+                    departure_date = input("choose departure date:").lower()
+                elif answer[:1].upper() == 'R':
+                    room_package = input("choose room:").lower()
+                else:
+                    print("Invalid entry")
+                x = 1
+            elif answer[:1].upper() == 'V':
+                print("""\nPlease enter Confirmation Number
+                """)
+                confirmation_number = input("Enter Confirmation Number: ").lower()
+                confirmed = b.Confirmation(confirmation_number)
+                if(confirmed):
+                    break
+            elif answer[:1].upper() == 'C':
+                print("""\nPlease enter Confirmation Number
+                """)
+                confirmation_number = input("Enter Confirmation Number: ").lower()
+                confirmed = b.Confirmation(confirmation_number)
+                if(confirmed):
+                    break
+            else:
+                print("Invalid entry")
+                x = 1
+            
+        x = 1
+        while True:
+
+            answer = input(
+                "\n\n\nLets make your Payment(payment): ").upper()
+            if answer[:1].upper() == 'P':
+                first_name = input("Enter first name:").lower()
+                last_name = input("Enter last name:").lower()
+                credit_card_number = input("Enter credit card number:").lower()
+                experation_date = input ("Enter experation date:").lower()
+                CCV = input("Enter CCV number:").lower()
+            else:
+                print("Invalid entry")
+                x = 1   
+    
+    
+    
     main()
 except(Exception, psycopg2.Error) as error:
     print("Error while fetching data from PostgreSQL", error)
